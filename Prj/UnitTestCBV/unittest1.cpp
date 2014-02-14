9ba7e50dd641a59ffeaddc83858df735fe4d15c7
Andrew <IsachenkoA@gmail.com>
2014-02-14 16:11:31 +0300

    added Assert Tests

--- Prj/UnitTestCBV/unittest1.cpp
+++ Prj/UnitTestCBV/unittest1.cpp
@@ -1,22 +1,55 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "../Common/BaseBool.h"
+#include <string>
 
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTestCBV
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(UnitTest)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestMethod_AssignDiz)
 		{
-			CBV a = CBV();
+			CBV a = CBV("00000000");
+			unsigned char x1 = 2;
+			unsigned char x2 = 4;
+			a.AssignDiz(8, &x1, &x2);
+			CString c = a.BitChar();
+			Assert::IsTrue(L"00000110" == c);
+		}
 
-			// TODO: Разместите здесь код своего теста
-			printf("hello");
+		TEST_METHOD(TestMethod_AssignCon)
+		{
+			CBV a = CBV("00000000");
+			unsigned char x1 = 6;
+			unsigned char x2 = 4;
+			a.AssignCon(8, &x1, &x2);
+			CString c = a.BitChar();
+			Assert::IsTrue(L"00000100" == c);
+		}
+
+		TEST_METHOD(TestMethod_AssignXor)
+		{
+			CBV a = CBV("00000000");
+			unsigned char x1 = 6;
+			unsigned char x2 = 4;
+			a.AssignXor(8, &x1, &x2);
+			CString c = a.BitChar();
+			Assert::IsTrue(L"00000010" == c);
+		}
+
+		TEST_METHOD(TestMethod_AssignDif)
+		{
+			CBV a = CBV("00000000");
+			unsigned char x1 = 6;
+			unsigned char x2 = 4;
+			a.AssignDif(8, &x1, &x2);
+			CString c = a.BitChar();
+			Assert::IsTrue(L"00000010" == c);
 		}
 
 	};
