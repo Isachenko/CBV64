47759bd229b8e79dd59eeac95325d8a9fbc3ea8e
kiryachok <kiryachoka@gmail.com>
2014-02-14 13:21:13 +0200

    unit tests
    fh

---
+++ Prj/UnitTestCBV/unittest1.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../Common/BaseBool.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestCBV
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			CBV a = CBV();
+
+			// TODO: Разместите здесь код своего теста
+			printf("hello");
+		}
+
+	};
+}
